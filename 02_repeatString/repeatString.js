e255044eefed8f52e939dda2bd5aa98d3a5cb74f
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2024-12-22 19:33:32 +0800

    Create a function that repeats a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (stringInput, count) {
+  if (count < 0) return "ERROR";
+  else {
+    let finalString = "";
+    for (let i = 0; i < count; i++) {
+      finalString += stringInput;
+    }
+    return finalString;
+  }
 };
 
 // Do not edit below this line
