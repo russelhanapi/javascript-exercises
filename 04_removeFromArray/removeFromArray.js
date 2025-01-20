5a418cfc5d8b138d0bc939a30c49f2af91bffb36
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-01-21 02:40:17 +0800

    Create a function that filters out specified elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...otherArguments) {
+  let result = arr.filter((item) => !otherArguments.includes(item));
+  return result;
 };
 
 // Do not edit below this line
