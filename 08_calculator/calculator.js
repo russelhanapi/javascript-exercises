a2806f759f4e93c73607101493a4651ede081fc8
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-01-21 18:47:39 +0800

    Create the add function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,24 @@
-const add = function() {
-	
+const add = function (...addends) {
+  const result = addends.reduce((sum, addend) => (sum += addend));
+  return result;
 };
 
-const subtract = function() {
-	
-};
+const subtract = function () {};
 
-const sum = function() {
-	
-};
+const sum = function () {};
 
-const multiply = function() {
+const multiply = function () {};
 
-};
+const power = function () {};
 
-const power = function() {
-	
-};
+const factorial = function () {};
 
-const factorial = function() {
-	
-};
+// The goal for this exercise is to create a calculator that does the following:
+
+// add, subtract, get the sum, multiply, get the power, and find the factorial
+
+// In order to do this please fill out each function with your solution. Make sure to return the value so you can test it in Jest! To see the expected value
+// take a look at the spec file that houses the Jest test cases.
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +27,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
