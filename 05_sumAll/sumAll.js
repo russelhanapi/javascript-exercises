003ed8534ea8d3538082ef3d1ef3b3c6761cef7b
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-01-21 18:01:11 +0800

    Create a function that determines whether or not a given year is a leap year

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -27,11 +27,3 @@ const sumAll = function (startRange, endRange) {
 
 // Do not edit below this line
 module.exports = sumAll;
-// ## Hints
-
-// Think about how you would do this on pen and paper and then how you might translate that process into code:
-// - make sure you pay attention to the function parameters
-// - create a variable to hold the final sum
-// - loop through the given numbers ([link](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration))
-// - on each iteration add the number to the sum
-// - return the sum after finishing the loop
