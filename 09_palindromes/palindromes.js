4b8751ceae07fcd8042bbc0e38f62c74580b2f73
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-01-22 10:29:44 +0800

    Create the palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const strArr = str
+    .toLowerCase()
+    .replaceAll(" ", "")
+    .replace(/[^a-zA-Z0-9 ]/g, "")
+    .split("");
+  let cont = [];
 
+  return strArr.every((el, i) => el === strArr[strArr.length - 1 - i], false);
 };
 
 // Do not edit below this line
