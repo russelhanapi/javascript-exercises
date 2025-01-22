543351e086a439ba248abd787b6141954be5a409
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-01-22 12:38:54 +0800

    Create a function that returns a specific member of the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
+  let fibonacciArr = [0, 1];
 
+  for (let i = 0; i <= n - 2; i++) {
+    fibonacciArr.push(
+      fibonacciArr[fibonacciArr.length - 1] +
+        fibonacciArr[fibonacciArr.length - 2]
+    );
+  }
+  return fibonacciArr[n];
 };
 
 // Do not edit below this line
