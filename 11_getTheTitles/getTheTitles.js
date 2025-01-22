30d694b2991cf23dd5101ae5b822694c15ead302
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-01-22 12:46:11 +0800

    Create a function that takes an array and returns an array of titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  const booksArr = books.map((book) => book.title);
+  return booksArr;
 };
 
+// getTheTitles(books) // ['Book','Book2']
 // Do not edit below this line
 module.exports = getTheTitles;
