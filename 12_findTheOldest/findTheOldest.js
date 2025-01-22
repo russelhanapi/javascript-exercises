608371b204112638f475171d1de274587a3beed4
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-01-22 15:37:52 +0800

    Create a function that will return the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = (people) =>
+  people.reduce((oldest, person) => {
+    const getAge = (p) =>
+      (p.yearOfDeath || new Date().getFullYear()) - p.yearOfBirth;
+    return getAge(person) > getAge(oldest) ? person : oldest;
+  });
 
 // Do not edit below this line
 module.exports = findTheOldest;
